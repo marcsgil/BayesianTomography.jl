baeb90427248983d7c3346b3cbcff2def2e8e4e1
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-01-24 19:23:26 -0300

    made plots

--- ExperimentalPhotocount/animation.jl
+++ ExperimentalPhotocount/animation.jl
@@ -1,13 +1,17 @@
 using HDF5, CairoMakie, BayesianTomography
 
 order = 1
-file = h5open("ExperimentalData/UFMG/Order$order/results.h5", "r")
+file = h5open("ExperimentalResults/UFMG/Order$order/results.h5", "r")
 history = read(file["6"])
 close(file)
 
-image = array_representation(history, (64, 64, 2))
+image = array_representation(history[1:1024], (64, 64, 2))
 image = vcat(image[:, :, 1], image[:, :, 2])
-heatmap(image, colormap=:hot)
+f = Figure(size=(500, 500))
+ax = Axis(f[1, 1], aspect=2)
+heatmap!(ax, image, colormap=:hot)
+f
+save("plot.svg", f)
 ##
 file = h5open("ExperimentalData/UFMG/Order$order/results.h5")
 nphotons = [length(read(file["$j"])) for j ∈ 1:50]
