a6b0b938ee4551663eac2d9ab6738ac447e1826c
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-03-19 10:29:06 -0300

    tried to organize stuff. didn't work yet

--- src/bayesian_inference.jl
+++ src/bayesian_inference.jl
@@ -1,117 +1,137 @@
-function log_likelihood(outcomes, operators, xs, cache)
-    mul!(cache, operators, xs)
-    map!(log, cache, cache)
-    outcomes ⋅ cache
-end
-
-function log_and_gradient!(outcomes, operators, xs, grad, cache1, cache2)
+function log_and_gradient!(outcomes, operators, xs, ∇ℓπ, cache1, cache2)
     mul!(cache1, operators, xs)
     map!(/, cache2, operators, cache1)
     map!(log, cache1, cache1)
-    mul!(grad, operators', cache2)
+    mul!(∇ℓπ, operators', cache2)
     outcomes ⋅ cache1
 end
 
-function h(z, v, grad, τ, A)
-    _grad = @view grad[begin+1:end]
-    (z ⋅ grad - v ⋅ grad - τ * dot(_grad, A, _grad) / 2) / 2
+function prepare_distribution!(μ, Σ, x, ∇ℓπ, σ, A)
+    BLAS.gemv!('N', σ^2 / 2, A, ∇ℓπ, false, μ)
+    μ .+= x
+    @. Σ = σ^2 * A
+    MvNormal((@view μ[begin+1:end]), @view(Σ[begin+1:end, begin+1:end]))
 end
 
-function proposal!(x, x₀, grad_x₀, τ, ρ, basis, Σ=I)
+function proposal!(x, dist)
+    _x = @view x[begin+1:end]
+    rand!(dist, _x)
+end
+
+function h(x, x₀, ∇ℓπ, σ, A)
+    _∇ℓπ = @view ∇ℓπ[begin+1:end]
+    _A = @view A[begin+1:end, begin+1:end]
+    (x ⋅ ∇ℓπ - x₀ ⋅ ∇ℓπ - σ^2 * dot(_∇ℓπ, _A, _∇ℓπ) / 4) / 2
+end
+
+function proposal_ratio(x, x₀, ∇ℓπ, ∇ℓπ₀, σ, A)
+    h(x, x₀, ∇ℓπ₀, σ, A) - h(x₀, x, ∇ℓπ, σ, A)
+end
+
+function acceptance!(x₀, x, ℓπ₀, ∇ℓπ₀, ∇ℓπ, f, σ, A)
+    ℓπ = f(x, ∇ℓπ)
+    if ℓπ₀ - ℓπ + proposal_ratio(x, x₀, ∇ℓπ, ∇ℓπ₀, σ, A) ≤ rand(Exponential())
+        @. x₀ = x
+        @. ∇ℓπ₀ = ∇ℓπ
+        return ℓπ, true
+    else
+        return ℓπ₀, false
+    end
+end
+
+function step!(x₀, x, ℓπ₀, ∇ℓπ₀, ∇ℓπ, f, σ, A, μ, Σ, ρ, basis, stats)
     is_in_domain = false
     not_in_domain_count = -1
+    T = typeof(σ)
+
     while !is_in_domain
-        _x₀ = @view x₀[begin+1:end]
-        _x = @view x[begin+1:end]
-        _grad_x₀ = @view grad_x₀[begin+1:end]
-        mul!(_x, Σ, _grad_x₀)
-        _x .*= τ
-        _x .+= _x₀
-        Random.rand!(MvNormal(_x, 2 * τ * Σ), _x)
+        dist = prepare_distribution!(μ, Σ, x₀, ∇ℓπ₀, σ, A)
+        proposal!(x, dist)
+
         is_in_domain = isposdef!(ρ, x, basis)
         not_in_domain_count += 1
-        """if not_in_domain_count > 10^4
-            τ /= 100
-        end"""
-    end
-    return not_in_domain_count, τ
-end
 
-function acceptance!(x₀, x, y₀, grad₀, grad, f, τ, Σ=Matrix{eltype(x)}(I, length(x) - 1, length(x) - 1))
-    y = f(x, grad)
-    if y₀ - y + h(x, x₀, grad₀, τ, Σ) - h(x₀, x, grad, τ, Σ) ≤ rand(Exponential())
-        @. x₀ = x
-        @. grad₀ = grad
-        return y, true
-    else
-        return y₀, false
-    end
-end
-
-function update_step_size(τ, acceptance_ratio, not_in_domain_ratio, target, m, M)
-    if target < acceptance_ratio && τ < M && not_in_domain_ratio < 10
-        return τ * 1.01
-    else
-        if τ > m
-            return τ * 0.99
-        else
-            return τ
+        if !is_in_domain && not_in_domain_count > 100
+            σ *= T(0.99)
         end
     end
-    @show τ
+
+    ℓπ₀, is_accepted = acceptance!(x₀, x, ℓπ₀, ∇ℓπ₀, ∇ℓπ, f, σ, A)
+    fit!(stats, x₀)
+
+    ℓπ₀, is_accepted, not_in_domain_count, σ
 end
 
-function metropolisHastings(f, x₀, nsamples, nwarm, τ; verbose=false)
+function update_step_size(σ, acceptance_ratio, target, min, max)
+    T = typeof(σ)
+    if target < acceptance_ratio && σ < max
+        return σ * T(1.01)
+    else
+        if σ > min
+            return σ * T(0.99)
+        else
+            return σ
+        end
+    end
+end
+
+function sample_markov_chain(f, x₀, nsamples, nwarm; verbose=false, σ=oftype(eltype(x₀), 1e-2))
     L = length(x₀)
     d = Int(√L)
-    basis = gell_man_matrices(d)
+
     target = 0.574
     minimum = 1e-8
-    maximum = 1
+    maximum = 100
 
     x = copy(x₀)
-    grad₀ = similar(x)
-    grad = similar(x)
-    y₀ = f(x₀, grad₀)
+    ∇ℓπ₀ = similar(x)
+    ∇ℓπ = similar(x)
+    ℓπ₀ = f(x₀, ∇ℓπ₀)
 
     T = eltype(x₀)
     ρ = Matrix{complex(T)}(undef, d, d)
+    basis = gell_man_matrices(d)
 
-    out_of_domain = 0
-    accepted = 0
-    stat = CovMatrix(T, L)
+    μ = similar(x)
+    A = Matrix{T}(I, L, L)
+    Σ = similar(A)
+
+    global_out_of_domain_count = 0
+    global_accepted_count = 0
+
+    stats = CovMatrix(T, L)
     for n ∈ 1:nwarm
-        not_in_domain_count, τ = proposal!(x, x₀, grad₀, τ, ρ, basis)
-        out_of_domain += not_in_domain_count
-        y₀, is_accepted = acceptance!(x₀, x, y₀, grad₀, grad, f, τ)
-        accepted += is_accepted
-        τ = update_step_size(τ, accepted / n, out_of_domain / n, target, minimum, maximum)
-        fit!(stat, x₀)
+        ℓπ₀, is_accepted, not_in_domain_count, σ = step!(x₀, x, ℓπ₀, ∇ℓπ₀, ∇ℓπ, f, σ, A, μ, Σ, ρ, basis, stats)
+        global_out_of_domain_count += not_in_domain_count
+        global_accepted_count += is_accepted
+        σ = update_step_size(σ, global_accepted_count / n, target, minimum, maximum)
     end
 
+    """copy!(A, cov(stats))
+    A *= d / tr(A)"""
 
-    Σ = @view cov(stat)[begin+1:end, begin+1:end]
-    Σ *= d / tr(Σ)
-    out_of_domain = 0
-    accepted = 0
+    global_out_of_domain_count = 0
+    global_accepted_count = 0
+
     stat = CovMatrix(T, L)
     for n ∈ 1:nsamples
-        if n % 100 == 0
-            Σ = @view cov(stat)[begin+1:end, begin+1:end]
-            Σ *= d / tr(Σ)
-        end
-        not_in_domain_count, τ = proposal!(x, x₀, grad₀, τ, ρ, basis)
-        out_of_domain += not_in_domain_count
-        y₀, is_accepted = acceptance!(x₀, x, y₀, grad₀, grad, f, τ)
-        accepted += is_accepted
-        τ = update_step_size(τ, accepted / n, out_of_domain / n, target, minimum, maximum)
-        fit!(stat, x₀)
+        """if n % 100 == 0
+            copy!(A, cov(stats))
+            A *= d / tr(A)
+        end"""
+        ℓπ₀, is_accepted, not_in_domain_count, σ = step!(x₀, x, ℓπ₀, ∇ℓπ₀, ∇ℓπ, f, σ, A, μ, Σ, ρ, basis, stats)
+        global_out_of_domain_count += not_in_domain_count
+        global_accepted_count += is_accepted
+        σ = update_step_size(σ, global_accepted_count / n, target, minimum, maximum)
+        @show x₀
+        @show ℓπ₀
     end
 
     if verbose
-        println("Out of domain rate: ", out_of_domain / nsamples)
-        println("Acceptance rate: ", accepted / nsamples)
-        println("τ: ", τ)
+        @info "Run information:"
+        println("Out of domain rate: ", global_out_of_domain_count / nsamples)
+        println("Acceptance rate: ", global_accepted_count / nsamples)
+        println("σ: ", σ)
     end
 
     stat
@@ -121,12 +141,10 @@ struct BayesianInference{T<:Real}
     povm::Matrix{T}
     nsamples::Int
     nwarm::Int
-    τ::T
-    function BayesianInference(povm::AbstractArray{Matrix{T2}}, nsamples, nwarm, τ=1.0f-3) where {T2}
-        T1 = real(T2)
+    function BayesianInference(povm::AbstractArray{Matrix{T}}, nsamples, nwarm) where {T}
         basis = gell_man_matrices(size(first(povm), 1))
         f(F) = real_orthogonal_projection(F, basis)
-        new{T1}(stack(f, povm, dims=1), nsamples, nwarm, τ)
+        new{real(T)}(stack(f, povm, dims=1), nsamples, nwarm)
     end
 end
 
@@ -143,18 +161,17 @@ function reduced_representation(povm, outcomes)
 end
 
 
-function prediction(outcomes, method::BayesianInference{T}; verbose=false) where {T}
+function prediction(outcomes, method::BayesianInference{T}; verbose=false, σ=T(1e-5)) where {T}
     reduced_povm, reduced_outcomes = reduced_representation(method.povm, outcomes)
 
     d = Int(√size(reduced_povm, 2))
     x₀ = zeros(T, d^2)
     x₀[begin] = 1 / √d
 
-    stats = CovMatrix(T, d^2)
     cache1 = similar(reduced_outcomes, float(eltype(reduced_outcomes)))
     cache2 = similar(cache1)
-    posterior(x, grad) = log_and_gradient!(reduced_outcomes, reduced_povm, x, grad, cache1, cache2)
-    stats = metropolisHastings(posterior, x₀, method.nsamples, method.nwarm, method.τ; verbose)
+    posterior(x, ∇ℓπ) = log_and_gradient!(reduced_outcomes, reduced_povm, x, ∇ℓπ, cache1, cache2)
+    stats = sample_markov_chain(posterior, x₀, method.nsamples, method.nwarm; verbose, σ)
 
     return linear_combination(mean(stats), gell_man_matrices(d)), cov(stats)
 end
