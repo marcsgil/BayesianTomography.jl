f175ad932ce45649a7ff713beb20a62c4969530e
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-08-22 13:06:27 -0300

    finished development of GellMann iterator and decomposition and reconstruction methods

--- develop_test.jl
+++ develop_test.jl
@@ -1,5 +1,11 @@
 using LinearAlgebra
 
+function nth_off_diagonal(n)
+    column = ceil(Int, (-1 + √(1 + 8n)) / 2)
+    row = n - column * (column - 1) ÷ 2
+    row, column + 1
+end
+
 function test_nth_off_diagonal()
     @assert nth_off_diagonal(1) == CartesianIndex(1, 2)
     @assert nth_off_diagonal(2) == CartesianIndex(1, 3)
@@ -13,25 +19,74 @@ function test_nth_off_diagonal()
     @assert nth_off_diagonal(10) == CartesianIndex(4, 5)
 end
 
-function nth_off_diagonal(n)
-    column = ceil(Int, (-1 + √(1 + 8n)) / 2)
-    row = n - column * (column - 1) ÷ 2
-    CartesianIndex(row, column + 1)
+struct GellMannMatrices{T<:Complex}
+    dim::Int
+end
+
+function GellMannMatrices(dim, ::Type{T}=ComplexF32) where {T<:Complex}
+    GellMannMatrices{T}(dim)
+end
+
+function Base.iterate(iter::GellMannMatrices{T}, state=1) where {T}
+    dim = iter.dim
+    state == dim^2 && return nothing
+
+    result = zeros(T, (dim, dim))
+
+    if state ≤ dim * (dim - 1) ÷ 2
+        i, j = nth_off_diagonal(state)
+        result[i, j] = 1 / √2
+        result[j, i] = 1 / √2
+    elseif state ≤ dim * (dim - 1)
+        i, j = nth_off_diagonal(state - dim * (dim - 1) ÷ 2)
+        result[i, j] = im / √2
+        result[j, i] = -im / √2
+    else
+        j = state - dim * (dim - 1)
+        factor = 1 / √(j * (j + 1))
+        for k ∈ 1:j
+            result[k, k] = factor
+        end
+        result[j+1, j+1] = -j * factor
+    end
+
+    result, state + 1
+end
+
+Base.IteratorSize(::GellMannMatrices) = Base.HasLength()
+Base.IteratorEltype(::GellMannMatrices) = Base.HasEltype()
+Base.eltype(::Type{GellMannMatrices{T}}) where {T} = Matrix{T}
+Base.length(iter::GellMannMatrices) = iter.dim^2 - 1
+
+function diag_retrival(n, θz)
+    result = zero(eltype(θz))
+    for j ∈ eachindex(θz)
+        factor = if n ≤ j
+            1
+        elseif n == j + 1
+            -j
+        else
+            0
+        end
+
+        result += θz[j] * factor / √(j^2 + j)
+    end
+    result
 end
 
 function gell_mann_projection!(θ::AbstractArray{T}, M) where {T}
     dim = size(M, 1)
     @assert length(θ) == dim^2 - 1
 
-    inv_sqrt2 = convert(T, inv(sqrt(2)))
+    sqrt2 = convert(T, sqrt(2))
 
     for n ∈ eachindex(θ)
         if n ≤ dim * (dim - 1) ÷ 2
             I = nth_off_diagonal(n)
-            θ[n] = inv_sqrt2 * real(M[I])
+            θ[n] = sqrt2 * real(M[I...])
         elseif n ≤ dim * (dim - 1)
             I = nth_off_diagonal(n - dim * (dim - 1) ÷ 2)
-            θ[n] = inv_sqrt2 * imag(M[I])
+            θ[n] = sqrt2 * imag(M[I...])
         else
             j = n - dim * (dim - 1)
             θ[n] = (sum(j -> M[j, j], 1:j) - j * M[j+1, j+1]) / convert(T, √(j * (j + 1)))
@@ -39,66 +94,67 @@ function gell_mann_projection!(θ::AbstractArray{T}, M) where {T}
     end
 end
 
-"""function vector_representation!(θ::AbstractArray{T}, M::Hermitian) where {T}
-    dim = size(M, 1)
-    @assert length(θ) == dim^2
-    
-    θ[1] = trace(M) / √dim
-    gell_mann_projection!((@view θ[2:end]), M)
-end"""
-
 function gell_mann_reconstruction!(M, θ::AbstractVector{T}) where {T}
     dim = size(M, 1)
     @assert length(θ) == dim^2 - 1
     fill!(M, zero(T))
 
-    sqrt2 = convert(T, sqrt(2))
+    inv_sqrt2 = convert(T, inv(sqrt(2)))
 
     n = 1
     # Off-diagonal elements (real part)
-    for j in 1:dim-1
-        for i in j+1:dim
-            M[i, j] = θ[n] * sqrt2
-            M[j, i] = θ[n] * sqrt2
-            n += 1
-        end
+    for k ∈ 1:dim * (dim - 1) ÷ 2
+        i, j = nth_off_diagonal(k)
+        M[i, j] = θ[n] * inv_sqrt2
+        M[j, i] = θ[n] * inv_sqrt2
+        n += 1
     end
 
     # Off-diagonal elements (imaginary part)
-    for j in 1:dim-1
-        for i in j+1:dim
-            M[i, j] -= im * θ[n] * sqrt2
-            M[j, i] -= im * θ[n] * sqrt2
-            n += 1
-        end
+    for k ∈ 1:dim * (dim - 1) ÷ 2
+        i, j = nth_off_diagonal(k)
+        M[i, j] += im * θ[n] * inv_sqrt2
+        M[j, i] -= im * θ[n] * inv_sqrt2
+        n += 1
     end
 
+    θz = @view θ[dim*(dim-1)+1:end]
     # Diagonal elements
-    for j in 1:dim-1
-        factor = convert(T, sqrt(j * (j + 1)))
-        diag_sum = sum(θ[k] * factor for k in dim*(dim-1)+1:dim*(dim-1)+j)
-        for i in 1:j
-            M[i, i] += diag_sum / factor
-        end
-        M[j+1, j+1] -= j * diag_sum / factor
+    for j in 1:dim
+        M[j, j] = diag_retrival(j, θz)
     end
 end
 
 function density_operator_reconstruction!(ρ, θ)
     gell_mann_reconstruction!(ρ, θ)
-    correction = 1 / size(ρ, 1)
-    for n ∈ axes(ρ, 1)
-        ρ[n, n] += correction
+    dim = size(ρ, 1)
+    factor = convert(eltype(ρ), 1 / dim)
+    for i ∈ 1:dim
+        ρ[i, i] += factor
     end
 end
 ##
-ρ = hermitianpart(rand(ComplexF32, 2, 2))
+dim = 10^3
+X = rand(ComplexF32, dim, dim)
+ρ = X * X'
 ρ /= tr(ρ)
-σ = Array{ComplexF32}(undef, size(ρ)...)
-θ = Vector{Float32}(undef, length(ρ) - 1)
+σ = Array{ComplexF32}(undef, dim, dim)
+θs = Vector{Float32}(undef, dim^2 - 1)
+ωs = GellMannMatrices(dim)
 
-θ_representation!(θ, ρ)
-density_operator_reconstruction!(σ, θ)
+@benchmark gell_mann_projection!($θs, $ρ)
 
-σ
+"""[real(tr(ρ * ω)) for ω in ωs] ≈ θs
+
+sum(prod, zip(θs, ωs)) .+ I(dim) ./ dim ≈ ρ"""
+
+
+@benchmark density_operator_reconstruction!($σ, $θs)
+
+density_operator_reconstruction!(σ, θs)
 ρ
+σ
+
+#sum(prod, zip(θs, ωs)) .+ I(dim) ./ dim ≈ ρ
+
+σ ≈ ρ
